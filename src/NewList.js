af356db744eb64b88acda6c055f56a14042a9b2e
kouchendi <koucxz@hotmail.com>
2018-04-24 23:37:09 +0800

    feature(newlist) add btn to dismiss list item

--- src/NewList.js
+++ src/NewList.js
@@ -1,47 +1,62 @@
 import React, { Component } from 'react';
 
 const list = [
-    {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://github.com/reactjs/redux',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    }
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
 ]
 
 class NewList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            list: list
-        }
-    }
-    render() {
-        return (
-            <div>
-                {this.state.list.map((item) =>
-                    <div key={item.objectID}>
-                        <span>
-                            <a href={item.url}>{item.title}</a>
-                        </span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                    </div>
-                )}
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      list
+    };
+  }
+
+  onDismiss = id => {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.list.map((item) =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+            <button
+              onClick={() => this.onDismiss(item.objectID)}
+              type="button"
+              >
+              Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default NewList;
