2e826e90732d6deaefea31a27f5e1ff1aec24599
kouchendi <koucxz@hotmail.com>
2019-06-23 19:05:58 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/static/',
+        '/static/media/',
+        '/static/js/'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
