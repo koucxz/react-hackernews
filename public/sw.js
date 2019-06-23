c7736669b7af14959a300b789ee0586a5bde5977
kouchendi <koucxz@hotmail.com>
2019-06-23 19:20:53 +0800

    change config

--- public/sw.js
+++ public/sw.js
@@ -2,16 +2,13 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/static/',
-        '/static/media/',
-        '/static/js/'
+        '/react-hackernews/'
       ]);
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
   event.respondWith(
       caches.match(event.request).then(function(response) {
           return response || fetch(event.request);
